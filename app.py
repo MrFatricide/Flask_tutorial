20327201a2d40653a22c2efd70d325beaa5015f7
Ken_sir <KLOW024@e.ntu.edu.sg>
2022-12-03 21:33:05 +0800

    Added table and formatted CSS

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)  # Represents current file name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+
+with app.app_context():  # Fix App Context Error
+    db = SQLAlchemy(app)
 
 
 class Todo(db.Model):
-    id = db.Column(db.integer, primary_key=True)
-    content = db.Column(db.string(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
