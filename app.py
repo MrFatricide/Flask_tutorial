4f5094308a68ca553e74816efb4bf36e063b95e6
Ken_sir <KLOW024@e.ntu.edu.sg>
2022-12-03 20:18:18 +0800

    Html and css base integration

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)  # Represents current file name
 
 
 @app.route('/')
 def index():
-    return 'Hello, World'
+    return render_template('index.html')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
